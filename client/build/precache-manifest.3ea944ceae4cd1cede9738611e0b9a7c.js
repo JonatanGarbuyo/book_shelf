0cd5ae611d4823157a417a7a714af19d83b7c8ed
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2020-04-14 16:29:31 -0300

    added build

---
+++ client/build/precache-manifest.3ea944ceae4cd1cede9738611e0b9a7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07f30aa14918e337aba512c1ba82e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc134e9ba27f10746525",
+    "url": "/static/js/2.3ba87d24.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.3ba87d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9277b843ac6b79e071c",
+    "url": "/static/js/main.6c7a807f.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
